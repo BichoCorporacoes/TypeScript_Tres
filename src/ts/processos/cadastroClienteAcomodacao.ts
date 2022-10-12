829797f39d608f4a1efb120bd9b95c06b54b5791
ZeroPirata <gabriel0gsbn@gmail.com>
2022-10-12 16:03:31 -0300

    Feat - Criação da vinculação do Cliente e acomodação

---
+++ src/ts/processos/cadastroClienteAcomodacao.ts
@@ -0,0 +1,39 @@
+import Processo from "../abstracoes/processo";
+import DiretorCasalSimples from "../diretores/diretorCasalSimples";
+import Armazem from "../dominio/armazem";
+import MenuTipoCadastroClienteAcomodacao from "../menus/menuTipoCadastroAcomodacaoCliente";
+import Acomodacao from "../modelos/acomodacao";
+import Cliente from "../modelos/cliente";
+
+export default class CadastroClienteAcomodacao extends Processo {
+  private cliente: Cliente;
+  private acomodacoes: Acomodacao[];
+  constructor(cliente: Cliente) {
+    super();
+    this.cliente = cliente;
+    this.menu = new MenuTipoCadastroClienteAcomodacao();
+    this.acomodacoes = Armazem.InstanciaUnica.Acomodacoes;
+  }
+
+  processar(): void {
+    this.menu.mostrar();
+    let entrada = this.entrada.receberNumero("Digite o numero do quarto");
+    this.acomodacoes.forEach((alvo, index) => {
+      switch (entrada) {
+        case index + 1:
+          if (entrada == index + 1) {
+            let acomodacao = new Acomodacao(
+              alvo.NomeAcomadacao,
+              alvo.CamaSolteiro,
+              alvo.CamaCasal,
+              alvo.Suite,
+              alvo.Climatizacao,
+              alvo.Garagem
+            );
+            this.cliente.Acomodacao = acomodacao;
+          }
+          break;
+      }
+    });
+  }
+}
